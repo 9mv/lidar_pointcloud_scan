c9ccfe209b1963782eff9f564103b325e6ba1744
Aleix Martinez <aleixmateumartinez@gmail.com>
2024-09-01 17:13:40 +0200

    Add subscription to scan in point cloud transformer and publish new point cloud (pending implementation)

--- include/lidar_pointcloud_scan/point_cloud_transformer.h
+++ include/lidar_pointcloud_scan/point_cloud_transformer.h
@@ -1,8 +1,17 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 #include "lidar_pointcloud_scan/msg/angle.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/point_cloud2_iterator.hpp"
+#include <math.h>
 #include "lidar_pointcloud_scan/types.h"
+
+#define RAD2DEG(x) ((x)*180./M_PI)
+#define DEG2RAD(x) ((x)*M_PI/180.)
+
+using PointCloud2 = sensor_msgs::msg::PointCloud2;
 
 class PointCloudTransformer : public rclcpp::Node
 {
@@ -11,9 +20,28 @@ public:
 
 // Private methods
 private:
-    void angle_update_callback (const lidar_pointcloud_scan::msg::Angle::SharedPtr msg);
+    void angleUpdateCallback (const lidar_pointcloud_scan::msg::Angle::SharedPtr msg);
+    void lidarScanCallback (const sensor_msgs::msg::LaserScan::SharedPtr scan);
+
+    void updatePointCloud(const sensor_msgs::msg::LaserScan::SharedPtr lidar_scan);
+    Result publishPointCloud();
 
 // Private attributes
 private:
-  rclcpp::Subscription<lidar_pointcloud_scan::msg::Angle>::SharedPtr subscription_;
+  rclcpp::Subscription<lidar_pointcloud_scan::msg::Angle>::SharedPtr angleSubscription_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidarScanSubscription_;
+
+  rclcpp::Publisher<PointCloud2>::SharedPtr pointCloudPublisher_;
+
+  // set to true while there is a scan in process
+  bool inScan_ = false;
+
+  // If reset is true, the point cloud is reseted
+  bool reset_ = false;
+
+  // Current angle of the lidar
+  int currentAngle_ = 0.0;
+
+  // Point cloud
+  PointCloud2 cumulativePointCloud_;
 };
--- src/point_cloud_transformer.cpp
+++ src/point_cloud_transformer.cpp
@@ -3,15 +3,46 @@
 PointCloudTransformer::PointCloudTransformer()
 : Node("point_cloud_transformer")
 {
-    subscription_ = this->create_subscription<lidar_pointcloud_scan::msg::Angle>(
-        "tilt_angle", 10, 
-        std::bind(&PointCloudTransformer::angle_update_callback, this, std::placeholders::_1));
+  angleSubscription_ = this->create_subscription<lidar_pointcloud_scan::msg::Angle>(
+      "tilt_angle", 10, 
+      std::bind(&PointCloudTransformer::angleUpdateCallback, this, std::placeholders::_1));
+
+  lidarScanSubscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "scan", 10, 
+      std::bind(&PointCloudTransformer::lidarScanCallback, this, std::placeholders::_1));
+
+  pointCloudPublisher_ = this->create_publisher<PointCloud2>("scan_3d", 10);
+
 }
 
-
-void PointCloudTransformer::angle_update_callback (const lidar_pointcloud_scan::msg::Angle::SharedPtr msg)
+void PointCloudTransformer::angleUpdateCallback (const lidar_pointcloud_scan::msg::Angle::SharedPtr msg)
 {
-    LOG_INFO(this, "New angle received: '%d'", msg->angle);
+  LOG_INFO(this, "New angle received: '%d'", msg->angle);
+  currentAngle_ = msg->angle;
+}
+
+void PointCloudTransformer::lidarScanCallback (const sensor_msgs::msg::LaserScan::SharedPtr scan)
+{
+  int count = scan->scan_time / scan->time_increment;
+
+  updatePointCloud(scan);
+
+  LOG_INFO(this, "New laserscan received: %s[%d]:", scan->header.frame_id.c_str(), count);
+}
+
+void PointCloudTransformer::updatePointCloud(const sensor_msgs::msg::LaserScan::SharedPtr lidar_scan)
+{
+  // TODO
+  publishPointCloud();
+}
+
+Result PointCloudTransformer::publishPointCloud()
+{
+  Result result = RESULT_OK;
+  LOG_INFO(this, "publishing new pointCloud");
+  pointCloudPublisher_->publish(cumulativePointCloud_);
+
+  return result;
 }
 
 int main(int argc, char * argv[])
